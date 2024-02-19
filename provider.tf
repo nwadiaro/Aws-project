4c41e551bebe27a42c0b6193afe5fcc8601bb133
kenneth <nwadiaro101charles231@gmail.com>
2024-02-18 19:32:20 -0500

    create main.tf, provider.tf, variable.tf

---
+++ provider.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region  = "us-east-1"
+  profile = "jack.roper"
+}
+
+resource "aws_instance" "example_server" {
+  ami           = "ami-04e914639d0cca79a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "JacksBlogExample"
+  }
+}
